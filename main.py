475b0d63350d0efbadab0b206898499249bfc71f
rsanghera <rsanghera1@gmail.com>
2024-11-26 14:40:02 -0500

    fps

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 
 #function to set the game screen
 def setScreen():
+    #setting fps
+    clock = pygame.time.Clock()
+    dt = 0
 
     #set display dimensions and create the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -27,6 +30,10 @@ def setScreen():
     running = True
     while running:
         for event in pygame.event.get():
+
+            #call the tick method to pause for define time
+            clock.tick(60)
+            dt = clock.tick() / 1000  
             if event.type == pygame.QUIT:
                 return
     
